2a7bbbcb4fcdccd336bdb593ee067a86e724ba0e
soniaprevost <so.prevost@gmail.com>
2015-03-18 19:01:23 +0100

    banner uploaded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+
+  resources :pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

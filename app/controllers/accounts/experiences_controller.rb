f2efd1ac3f1e41a7ecf6241c1c7ef9edd94ba4cb
soniaprevost <so.prevost@gmail.com>
2015-09-01 14:20:49 +0200

    create view for index and new experiences

--- app/controllers/accounts/experiences_controller.rb
+++ app/controllers/accounts/experiences_controller.rb
@@ -1,9 +1,18 @@
 class Accounts::ExperiencesController < ApplicationController
 
   def index
+    @experiences = Experience.all
+    @experience  = Experience.new
   end
 
   def create
+    @experience = Experience.new(experience_params)
+
+    if @experience.save
+      redirect_to account_experiences_path
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -12,4 +21,9 @@ class Accounts::ExperiencesController < ApplicationController
   def destroy
   end
 
+  private
+
+  def experience_params
+    params.require(:experience).permit(:user_id, :job_title, :job_content, :start_date, :end_date, :company_name, :company_link)
+  end
 end

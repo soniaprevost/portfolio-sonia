528c7c11631680ca1bdde1195b09f903ea6a4289
soniaprevost <so.prevost@gmail.com>
2015-09-01 13:42:23 +0200

    generate experiences controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :pages
 
   resource :account, only: [:show] do
-    resources :experiences, module: :accounts
+    resources :experiences, only: [:index, :create, :update, :destroy], module: :accounts
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
